ee6e13aa24a81fe3b0ef383b3c28edfedc862f79
cmascart <mascart@i3s.unice.fr>
2017-09-14 11:33:13 +0200

    Removed files on . that have been put in Java-implementation/

--- src/Util/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * This package contains passive classes that serves as a scaffolding for the simulator. Their behavior is quite simple and generic, making them highly reusable for the same kind of systems.
- * <p>
- * They are mainly used by {@link Simulator} and {@link Network}.
- */
-package Util;
